92e68804d151dac689dbd251cadcfc5dda725174
Ian Ribalta Gené <ian.ribalta@gmail.com>
2023-12-16 14:34:51 +0100

    added text and explanation to CardData

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,6 +1,9 @@
 class CardData{
   final int id;
   final String name;
+  final String text;
+  final String information;
+  final bool informative;
   final void left;
   final void right;
   final String img;
@@ -8,6 +11,9 @@ class CardData{
   const CardData({
     required this.id,
     required this.name,
+    required this.text,
+    required this.information,
+    required this.informative,
     required this.left,
     required this.right,
     required this.img,
@@ -15,16 +21,18 @@ class CardData{
 }
 
 const info = [
-  CardData(id: 0, name: "Initial", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 1, name: "Drunk", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 2, name: "Drive", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 3, name: "Sex", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 4, name: "Death", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 5, name: "AIDS", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 6, name: "Chlamydia", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 7, name: "Gonorrhea", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 8, name: "Syphilis", left: dummy, right: dummy, img: "./img"),
-  CardData(id: 9, name: "Genital herpes", left: dummy, right: dummy, img: "./img"),
+  CardData(id: 0, name: "Initial", text: "", information:"", informative: false, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 1, name: "Drunk", text: "", information:"", informative: false, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 2, name: "Drive", text: "", information:"", informative: false, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 3, name: "Sex", text: "", information:"", informative: false, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 4, name: "Death", text: "", information:"", informative: false, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 5, name: "SIDA", text: "", information:"El SIDA es una una malaltia en la qual es produeix una pèrdua severa de la immunitat cel·lular del cos, reduint molt la resistència a la infecció i la malignitat. ", informative: true, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 6, name: "Clamídia", text: "", information:"La clamídia és una ETS comuna que pot causar infecció tant en homes com en dones. Pot causar danys permanents al sistema reproductor de la dona.", informative: true, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 7, name: "Gonorrea", text: "", information:"Els homes infectats poden experimentar dolor o ardor en orinar, secreció del penis o dolor testicular. Les dones infectades poden experimentar ardor en orinar, secreció vaginal, sagnat vaginal entre períodes o dolor pèlvic.", informative: true, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 8, name: "Sífilis", text: "", information:"La sífilis es transmet durant el sexe oral, vaginal i anal, durant l'embaràs i per transfusió de sang. La sífilis durant l'embaràs pot provocar morts, mort de nounats i nadons nascuts amb sífilis (sífilis congènita).", informative: true, left: dummy, right: dummy, img: "./img"),
+  CardData(id: 9, name: "Genital herpes", text: "", information:"Sovint, l'herpes genital es pot transmetre pel contacte pell a pell durant l'activitat sexual. Algunes persones infectades amb el virus poden tenir símptomes molt lleus o no presentar cap símptoma. La infecció pot provocar butllofes o úlceres doloroses que poden repetir-se amb el temps.", informative: true, left: dummy, right: dummy, img: "./img"),
 ];
 
 void dummy(){}
+
+
