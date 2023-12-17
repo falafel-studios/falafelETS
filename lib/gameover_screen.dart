0c419a4f2b36cc9897f554b31c74724a42cf41ad
sergisi <sergisiba@gmail.com>
2023-12-17 11:45:48 +0100

    Improved TM the GameOver.

--- lib/gameover_screen.dart
+++ lib/gameover_screen.dart
@@ -39,31 +39,27 @@ class GameOverScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 60),
               ),
             ),
-            Card(
-              margin:
-                  const EdgeInsets.only(left: 0, top: 150, right: 0, bottom: 0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        var cardStore =
-                            Provider.of<CardStore>(context, listen: false);
-                        var pointStore =
-                            Provider.of<PointStore>(context, listen: false);
-                        cardStore.sumIndex();
-                        pointStore.setPoints(Points(
-                            health: 50, money: 50, energy: 50, mental: 50));
-                      },
-                      style: ElevatedButton.styleFrom(elevation: 2),
-                      child: const Text(
-                        'Torna-hi a jugar',
-                        style: TextStyle(color: Colors.black),
-                      ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      var cardStore =
+                          Provider.of<CardStore>(context, listen: false);
+                      var pointStore =
+                          Provider.of<PointStore>(context, listen: false);
+                      cardStore.sumIndex();
+                      pointStore.setPoints(Points(
+                          health: 50, money: 50, energy: 50, mental: 50));
+                    },
+                    style: ElevatedButton.styleFrom(elevation: 2),
+                    child: const Text(
+                      'Torna-hi a jugar',
+                      style: TextStyle(color: Colors.black),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
